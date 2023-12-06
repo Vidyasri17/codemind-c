0fbbe69e85c4ae546c357cbcf29be7404d4d096f
Vidyasri17 <147472710+Vidyasri17@users.noreply.github.com>
2023-12-06 15:59:41 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int arr[n];     for(int i = 0 ; i < n ; i++)     {         scanf("%d",&arr[i]);     }     int a = 0,b = 0 ;     for(int i = 0 ; i < n ; i++)     {         a = (a + arr[i]);     }     int c = a/n;     for(int i = 0 ; i < n ; i++)     {         if(arr[i]<=c)         {             b++;         }     }     printf("%d",b); }

---
+++ Elements_smaller_than_average.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i = 0 ; i < n ; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int a = 0,b = 0 ;
+    for(int i = 0 ; i < n ; i++)
+    {
+        a = (a + arr[i]);
+    }
+    int c = a/n;
+    for(int i = 0 ; i < n ; i++)
+    {
+        if(arr[i]<=c)
+        {
+            b++;
+        }
+    }
+    printf("%d",b);
+}
