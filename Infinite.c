becf83520c66f0e6fbdc79537883c84565b1f055
Vidyasri17 <147472710+Vidyasri17@users.noreply.github.com>
2023-12-20 09:52:36 +0530

    #include<stdio.h> int main() {     int n;     while(n!=-1)     {         scanf("%d",&n);         if(n==-1)         {             break;         }         printf("%d",n*n);         printf(" ");     } }

---
+++ Infinite.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    while(n!=-1)
+    {
+        scanf("%d",&n);
+        if(n==-1)
+        {
+            break;
+        }
+        printf("%d",n*n);
+        printf("
+");
+    }
+}
