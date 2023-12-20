3502a6d512bbb8481cb3d61f697830ce5c7dba36
Vidyasri17 <147472710+Vidyasri17@users.noreply.github.com>
2023-12-20 10:44:41 +0530

    #include<stdio.h> int main() {     int a , b ;     scanf("%d %d",&a,&b);     for(int i = a ; i > 0 ; i--)     {         if((a%i==0)&&(b%i==0))         {             printf("%d",i);             break;         }     } }

---
+++ Greatest_Common_Divisor.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a , b ;
+    scanf("%d %d",&a,&b);
+    for(int i = a ; i > 0 ; i--)
+    {
+        if((a%i==0)&&(b%i==0))
+        {
+            printf("%d",i);
+            break;
+        }
+    }
+}
